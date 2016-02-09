c546268976028679cc8b7cc3e31e96e1d48556fd
Gamernew <jessyhurbain@gmail.com>
2016-02-09 13:44:47 +0100

    add js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-var usage = document.getElementsByName('taille').value;
-console.log(usage);
+function check(tab){
+for (i=0;i<tab.length;i++)
+{
+	if(tab[i].checked)
+	{
+		var s = tab[i].value;
+    console.log(s);
+		break;
+	}
+}
+}
